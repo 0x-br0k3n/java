633382b1976a641d1395168312485ec2d3092a2f
br0k3n <127822226+0x-br0k3n@users.noreply.github.com>
2025-05-26 13:39:47 +0530

    Update config.ts

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vitepress'
+<<<<<<< Updated upstream
 import head from "./head"
 
+=======
+
+// https://vitepress.dev/reference/site-config
+>>>>>>> Stashed changes
 export default defineConfig({
   title: "Java",
   description: "Java Notes",
   base: '/java/',
+<<<<<<< Updated upstream
   head,
   markdown: {
     math: true
@@ -21,10 +27,18 @@ export default defineConfig({
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Contribue to this project', link: '/contribute' }
+=======
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/credits' }
+>>>>>>> Stashed changes
     ],
 
     sidebar: [
       {
+<<<<<<< Updated upstream
         text: 'Introduciton',
         collapsed: false,
         items: [
@@ -46,6 +60,12 @@ export default defineConfig({
         items: [
           { text: 'Laws', link: '/boolean-algebra/Laws' },
           { text: 'K-map', link: '/boolean-algebra/K-map' }
+=======
+        text: 'Examples',
+        items: [
+          { text: 'About Java', link: '/intro/About Java' },
+          //{ text: 'Runtime API Examples', link: '/api-examples' }
+>>>>>>> Stashed changes
         ]
       }
     ],
